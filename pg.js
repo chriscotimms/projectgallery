277f6c46c6dc71d2578fa5751d5b67a364333152
Chris Timms <chriscotimms@gmail.com>
2023-02-13 17:27:48 +0000

    resizing using external css media responses

--- pg.js
+++ pg.js
@@ -1,1 +1,50 @@
-console.log('hello');
+const note = document.querySelectorAll('.initial');
+console.log(note);
+
+
+/*
+const showImage = () => 
+{
+  document.getElementById("first").classList.toggle('hidden');
+}
+*/
+
+const showImage = (x) => 
+{
+  document.getElementById(x).classList.toggle('hidden');
+  console.dir(document.getElementById(x));
+  console.dir(x);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+let value = document.getElementsByTagName("article");
+console.log(value);
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+*/
+
