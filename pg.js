a4a013574776838d2957cd8883b62611b4569740
Chris Timms <chriscotimms@gmail.com>
2023-04-05 15:31:11 +0100

    github wouldnt display page and needed to be reset

--- pg.js
+++ pg.js
@@ -2,97 +2,45 @@ const initial = document.querySelectorAll('.initial');
 const image = document.querySelectorAll('img');
 const btnbtn = document.querySelectorAll('.btn');
 
-const focusEvent = (e) => {
-  let temp = e.target.querySelector('.description');
-  temp.classList.toggle('hidden');
-}
 
-const unfocusEvent = (e) => {
-  let temp = e.target.querySelector('.description');
-  temp.classList.add('hidden');
-}
-
-const revealText = (e) => {
-  let pp = e.target.parentNode.querySelector('.pp');
-  pp.classList.toggle('hidden');
-  pp.classList.toggle('adclass');
-}
 
 function addListen() {
 initial.forEach((item) => {
-  item.addEventListener('focusin', focusEvent);
-  item.addEventListener('mouseenter', focusEvent);
-  item.addEventListener('focusout' , unfocusEvent);
-  item.addEventListener('mouseleave' , unfocusEvent);
+  item.addEventListener('focusin', (event) => {
+    let temp = event.currentTarget.querySelector('.description');
+    temp.classList.remove('hidden');
+  });
+  item.addEventListener('mouseenter', (event) => {
+    let temp = event.currentTarget.querySelector('.description');
+    temp.classList.remove('hidden');
+  });
+  item.addEventListener('focusout' , (event) => {
+    let temp2 = event.currentTarget.querySelector('.description');
+    temp2.classList.add('hidden');
+  item.addEventListener('mouseleave' , (event) => {
+    let temp2 = event.currentTarget.querySelector('.description');
+    temp2.classList.add('hidden');
+});
+  });
 });
 };
 
+
+function addBtn() {
 btnbtn.forEach((item) => {
-  item.addEventListener('click', revealText);
-})
-
-
-
-document.addEventListener('DOMContentLoaded', addListen, false);
-  
-
-
-
-
-
-/*
-
-
-//////previous code
-
-
-//button function, x = Div id ("first", "second", "third")
-const showImage = (x) => {
-
-//if new function input is not the same as previous input
-if (x !== btnSel) {
-
-  note.forEach ((element) => { //access each article  
-    element.classList.remove('initialenlarge'); //remove any 'initialenlarge' class  
-    element.classList.add('initialshrink'); //and shrink with 'initialshrink' class
-  })
-
-  const vis = document.querySelectorAll('.visible'); //access inner div class
-  vis.forEach ((element) => { //for each div with visible class
-    element.classList.remove('visible'); //remove visible class
-    element.classList.add('hidden'); //add hidden class
-  })
-
-  btnSel = x; //update btnSel as current selection
-
-//or if new function id input is not the same as previous input
-} else {
-
-  const intShr = document.querySelectorAll('.initialshrink'); //access all <article>s with shrink class
-  intShr.forEach ((element) => { //for each:
-    element.classList.remove('initialshrink'); //remove shrink class
-    element.classList.add('initial'); //reset to inital class (medium sizing) for easier browsing
-  })
-
-  btnSel = 'rifts'; //update btnSel to force next function iteration to trigger condition (!==) of if statement above
-
-}
-  document.getElementById(x).classList.toggle('hidden');
-  document.getElementById(x).classList.toggle('visible');
-  document.getElementById(x).parentElement.classList.add('initial');
-  document.getElementById(x).parentElement.classList.remove('initialshrink');
-  document.getElementById(x).parentElement.classList.toggle('initialenlarge');
-}
+    item.addEventListener('click', (event) => {
+    let pp = event.currentTarget.parentNode.querySelector('.pp');
+    pp.classList.toggle('hidden');
+    pp.classList.toggle('adclass');
+    event.stopImmediatePropagation();
+  }, true);
+});
+};
 
 
 
 
+document.addEventListener('DOMContentLoaded', addListen);
+document.addEventListener('DOMContentLoaded', addBtn);
 
 
-
-
-
-
-
-
-*/
